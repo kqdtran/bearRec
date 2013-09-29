01f0a7a691ae15df88e1541d18d5e243518af7a9
Khoa Tran <khoatran@berkeley.edu>
2013-09-28 19:10:51 -0700

    CLI version for similarity and basic jQuery for client-side

---
+++ project/cli/tfidf.py
@@ -0,0 +1,40 @@
+import os
+from pattern.vector import Document, Model, TFIDF, LEMMA
+
+def readFile(filename):
+  """Read a text file with the given file name and return
+  its content"""
+
+  with open("../../data/" + filename, "r") as f:
+    return " ".join(f.readlines()).strip()
+
+def runTFIDF():
+  """Given a list of classes, construct a Vector-Space Model.
+  We only need to do it once and save it to a pickle file for
+  fast loading later on"""
+
+  model = Model(documents=[], weight=TFIDF)
+  for r, d, files in os.walk("../../data"):
+    for f in files:
+      if f.endswith(".txt"):
+        text = readFile(f)
+        doc = Document(text, stemmer=LEMMA, stopwords=True, name=f.replace(".txt", ""))
+        model.append(doc)
+  model.save("../../pickle/course.pic")
+
+def loadTFIDF():
+  """Load the pickle file created by run TFIDF"""
+
+  return Model.load("../../pickle/course.pic")
+
+def findSimilarity(model, term):
+  """Find the similarity between the given term and 
+  any of the vectors in the space"""
+
+  doc = Document(term)
+  return model.neighbors(doc)
+
+if __name__ == "__main__":
+  #runTFIDF()
+  model = loadTFIDF()
+  print findSimilarity(model, "algorithm")
--- project/static/js/script.js
+++ project/static/js/script.js
@@ -1,4 +1,69 @@
 (function() {
   var TIMEOUT = 20000;
   var loadingImg = '<img id="loadingImage" src="/static/img/ajax-loader.gif" />';
+
+  var errored = function(xml, status, message, $elem) {
+    if (status === "timeout") {
+      $elem.html("Timed out. Try again.");
+    } else {
+      $elem.html("Something went wrong. Try again.");
+    }
+    return null;
+  };
+
+  var updateValue = function(url, text, $elem, success) {
+    $elem.append(loadingImg);
+    $.ajax({
+      url: url,
+      type: "POST",
+      timeout: TIMEOUT,
+      data: {"text": text},
+      dataType: "json",
+      success: success,
+      error: function(xml, status, message) {
+        errored(xml, status, message, $elem);
+      }
+    });
+  };
+
+  $("#findTerm").on('click', function(e) {
+    e.preventDefault();
+    var text = $("#searchTermBox").val();
+    //scrapeSimilarCourses(text);
+    console.log(text);
+  });
+
+  var scrapeSimilarCourses = function(text) {
+    var $sentDiv = $("#termDiv");
+    $sentBtn = $("#findTerm");
+    text = text.trim();  // trim whitespace from both ends
+
+    if ($sentDiv.is(":visible")) {
+      $sentDiv.hide();
+      $sentBtn.removeClass("active");
+    } else {
+      $sentDiv.show();
+      var $sentTable = $("#termDiv table");
+      var $tbody = $sentTable.children("tbody");
+      updateValue("/findSimilarCoursestoTerm", text, $tbody, function(res) {
+        $sentBtn.addClass("active");
+        $tbody.empty();
+        var sentences = res.result;
+        if (sentences.length <= 0 || !text.trim()) {
+          $tbody.append("<tr>" +
+            "<td>No such course</td>" +
+            "<td>0</td>" +
+            "</tr>");
+        } else {   
+          sentences.forEach(function(elem, index) {         
+            $tbody.append("<tr>" + 
+              "<td>" + elem.course + "</td>" +
+              "<td>" + elem.score +"</td>" + 
+              "</tr>");
+          });
+        }
+      });
+    }
+  };
+
 }).call(this);
